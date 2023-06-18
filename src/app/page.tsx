a425adcaa3bf519dae687609f61900fcfe9cd9a1
bhavanad05 <bhavana.nd05@gmail.com>
2023-06-18 20:49:57 +0530

    changes for dashboad screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,7 +120,7 @@ export default function Example() {
         {/* Hero section */}
         <div className="relative isolate overflow-hidden bg-gray-900 pb-16 pt-14 sm:pb-20">
           <img
-            src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+            src="https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHNwYWNlfGVufDB8fDB8fHww&auto=format&fit=crop&w=2000&q=100&blend=111827&sat=-100&exp=15&blend-mode=multiply"
             alt=""
             className="absolute inset-0 -z-10 h-full w-full object-cover"
           />
